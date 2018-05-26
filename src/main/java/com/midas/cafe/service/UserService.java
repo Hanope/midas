15e90ac5b8f64b899ccf1e28621ad972db92ef80
lyh4967 <lyh4965@naver.com>
2018-05-26 18:58:44 +0900

    "login ready"

--- src/main/java/com/midas/cafe/service/UserService.java
+++ src/main/java/com/midas/cafe/service/UserService.java
@@ -1,11 +1,24 @@
 package com.midas.cafe.service;
 
+import com.midas.cafe.model.User;
+import com.midas.cafe.repository.user.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * User: kimkm
  * Date: 2018-05-26
  * Time: 오후 4:01
  */
-public class UserService
+@Service
+public class UserService {
 
-{
+    @Autowired
+    UserDao userDao;
+
+    public int joinUser(User user){
+        return userDao.insertUser(user);
+    }
+
+
 }
